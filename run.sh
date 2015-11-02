4a68c952625acdc033d73098e3642131874527e4
Nalam V S S Krishna Chaitanya <cs13b021@smail.iitm.ac.in>
2015-11-02 16:23:12 +0530

    Test script is added.

---
+++ run.sh
@@ -0,0 +1,48 @@
+#
+# CS2110 Testbench
+# Runs tests
+
+TEST_DIR=public
+TEST_COUNT=10
+PROG=a.out
+ARGS=
+
+./$PROG < sssp.kc > sssp.c;
+gcc sssp.c -o b.out;
+./$PROG < dfs.kc > dfs.c;
+gcc dfs.c -o c.out;
+
+j=0
+for i in `seq 1 $TEST_COUNT`;
+do
+    echo -n "Running SSSP $i... "
+	inp="input/$i.txt";
+    outs="ssspmy/$i.ans";
+	acts="sssp/SSSP_$i.txt";
+	./b.out $TEST_DIR/$inp > $TEST_DIR/$outs;
+    diff -b $TEST_DIR/$outs $TEST_DIR/$acts > /dev/null;
+    if [[ $? == 0 ]] ; then
+        echo "Passed."
+        j=$((j+1))
+    else
+        echo "Failed."
+    fi
+done;
+echo "Done ($j/$i) passed in SSSP.";
+j=0
+for i in `seq 1 $TEST_COUNT`;
+do
+    echo -n "Running DFS $i... "
+	inp="input/$i.txt";
+    outd="dfsmy/$i.ans";
+    actd="dfs/dfs_$i.txt";
+	./c.out $TEST_DIR/$inp > $TEST_DIR/$outd;
+    diff -b $TEST_DIR/$outd $TEST_DIR/$actd > /dev/null;
+    if [[ $? == 0 ]] ; then
+        echo "Passed."
+        j=$((j+1))
+    else
+        echo "Failed."
+    fi
+done;
+echo "Done ($j/$i) passed in DFS.";
